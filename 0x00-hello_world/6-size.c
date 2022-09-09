807fe8a54bac2187063213202287c2a59e36c425
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-08 19:45:23 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float floatType;
 	/*sizeof evaluates the size of a variable*/
 	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
-	printf("Size of a int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongintType));
 	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
