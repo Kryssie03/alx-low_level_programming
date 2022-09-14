a994c42db6ff779ecf1a6e87170f339e68011d45
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-14 15:43:00 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@
 void print_times_table(int n)
 {
 	int num, multi, prod;
-	
+
 	for (num = 0; num <= n; num++)
 	{
 	       _putchar('0');
@@ -28,8 +28,8 @@ void print_times_table(int n)
 				_putchar(' ');
 			}
 			else
-				_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
