1ae7d9b2fdeb03b4be251344e8f273baf929c9cc
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-14 16:04:46 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,9 +28,9 @@ void print_times_table(int n)
 				_putchar(' ');
 			}
 			else
-				_putchar((prod / 10) + '0');
+				_putchar((prod / 10));
 
-			_putchar((prod % 10) + '0');
+			_putchar((prod % 10));
 		}
 		_putchar('\n');
 	}
