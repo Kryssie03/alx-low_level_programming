8695d2fd64bfc985e31d078d4cd63e01c7abf053
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-14 13:35:05 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,19 +8,23 @@ void jack_bauer(void)
 {
 	int min = 0;
 	int hours = 0;
-	int min_remainder = min % 10;
-	int hours_remainder = hours % 10;
+	int min_remainder;
+	int hours_remainder;
 
-	for (min = '0'; min <= 59; min++)
+	while (hours <= 23)
 	{
-		for (hours = '0'; hours <= 23; hours++)
-		{
-			_putchar((hours / 10) + '0');
-			_putchar(hours_remainder + '0');
-			_putchar(':');
-		}
-		_putchar((min / 10) + '0');
-		_putchar(min_remainder + '0');
-	}
+	while (min <= 59)
+	{
+	min_remainer = min % 10;  hours_remainder = hours % 10;
+	_putchar(hours / 10 + '0');
+	_putchar(hours_remainder + '0');
+	_putchar(':');
+	_putchar(min / 10 + '0');
+	_putchar(min_remainder + '0');
+	min++;
 	_putchar('\n');
+	}
+	hours++;
+	min = 0;
+}
 }
