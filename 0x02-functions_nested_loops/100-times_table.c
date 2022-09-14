e2a7142c14ba689656a38d2a02ccab8301371a8b
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-14 16:41:15 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,13 +30,15 @@ void print_times_table(int n)
 			else if (prod > 99)
 			{
 				_putchar((prod / 100) + '0');
-				_putchar(((prod % 10) % 10) + '0');
 				_putchar((prod % 10) + '0');
 			}
+			 else if (prod > 9)
+			 {
+				 _putchar((prod / 10) + '0');
+			       	_putchar((prod % 10) + '0');
+			 }
 			 else
-				 _putchar(((prod / 10) % 10) + '0');
-
-			_putchar((prod % 10) + '0');
+				 _putchar(prod + '0');
 		}
 		_putchar('\n');
 	}
