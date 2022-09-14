36c83e8703012db109bccb0b2aadee518f36ef91
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-13 17:52:57 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  *print_alphabet - function to print alphabets in lowercase
  *
- * Return: Always 0
+ * Return: 0
  */
 void print_alphabet(void)
 {
@@ -10,7 +10,7 @@ void print_alphabet(void)
 
 	for (c='a'; c<='z'; c++)
 	{
-	      	_putchar(c);
-	} 	
+	       _putchar(c);
+	}
 _putchar('\n');
 }
