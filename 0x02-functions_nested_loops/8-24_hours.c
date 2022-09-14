f179df41bc883f82e19fdef8fd2d7e5f466c040d
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-14 12:45:43 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,6 @@
 /**
  * jack_bauer - funtion to print every minutes of the day starting from 00:00 
  * to 23:59
- *
  * Return: 0
  */
 void jack_bauer(void)
@@ -12,9 +11,9 @@ void jack_bauer(void)
 	int min_remainder = min % 10;
 	int hours_remainder = hours % 10;
 
-	for (min = '0'; min <= '59'; min++)
+	for (min = '0'; min <= 59; min++)
 	{
-		for (hours = '0'; hours <= '23'; hours++)
+		for (hours = '0'; hours <= 23; hours++)
 		{
 			_putchar((hours / 10) + '0');
 			_putchar(hours_remainder + '0');
