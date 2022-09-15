e0c0af41744222fe23b3b9e55402598b793c6fbe
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-14 18:52:08 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,8 +11,10 @@ int main(void)
 	for (i = 0; i < 1024; i++)
 	{
 		if (((i % 3) == 0) || ((i % 5) == 0))
-		       sum +=i;
+		{
+		      sum += i;
+		}
 	}
-	printf("%d/n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
