e66e07faacc26c07141d2133168b5ff8b622ba23
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-14 20:03:42 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	int n1 = 0;
-	int n2 = 1;
-	int n3;
+	unsigned long n1 = 0, n2 = 1, n3;
 	int i;
 
 	for (i = 0; i < 50; ++i)
 	{
 		n3 = n1 + n2;
-		printf("%d, ", n3);
+		printf("%lu, ", n3);
+
 		n1 = n2;
 		n2 = n3;
 	}
