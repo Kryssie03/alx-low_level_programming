a3417d3a1f85fa4140cbe08b3bc6d79b22046c9a
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-15 20:04:40 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef HOLBERTON_H_INCLUDED                                                                                                            
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H                                                                                                    
+#define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,5 +13,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 
-#endif
+#endif /* MAIN_H */
