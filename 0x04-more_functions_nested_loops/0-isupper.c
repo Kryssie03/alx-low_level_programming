0641c4fa03ce069e73edfaf234c30c051d9d583b
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-15 20:09:03 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isupper - function to check for upper case characters
+ *
  * @c: argument of function
  *
- * Return: return 1 if its upppercase and return 0 otherwise
+ * Return: 1 if its upppercase and 0 otherwise
  */
 
 int _isupper(int c)
 {
-
 	if (c >= 65 && c <= 90)
 	{
 		return (1);
