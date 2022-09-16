6b356bb202ed2d8738e39fa98120d7a993020175
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-15 19:43:47 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,11 +10,14 @@ int _isupper(int c)
 {
 	int c;
 	
-	if (c == 'A' && c <= 'Z')
+	for (c = 'A'; c <= 'Z'; c++)
 	{
-		return (1);
+		if (c == 'A' && c <= 'Z')
+		{
+			return (1);
+		}
+		else
+			return (0);
 	}
-	else
-		return (0);
 }
  
