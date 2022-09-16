18b7565c69c49c538f0a112f87715935909f5962
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-16 16:16:21 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_numbers - function to print numbers 0 to 9
@@ -10,8 +11,8 @@ void print_numbers(void)
 {
 	int c;
 
-	for (c >= 0; c <= 9; c++)
-		_putchar(c);
+	for (c >= 48; c <= 57; c++)
+		putchar(c);
 	
-	_putchar('\n');
+	putchar('\n');
 }
