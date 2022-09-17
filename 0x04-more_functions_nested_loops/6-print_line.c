b077b7a37fac1fe8d7e53d9aef52a5120be8b47f
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-16 17:29:26 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - function to print line
+ * @n: argument of function
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int n;
+
+	for (n > 0; n <= n; n++)
+	{
+		_putchar('_');
+
+		if (n <= 0)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
+
