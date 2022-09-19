7aa29d64ec679390b606de5d087f3895f316561c
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-19 16:25:34 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - function to print string in reverse
+ * @s: function parameter
+ */
+
+void print_rev(char *s)
+{
+	int k = 0;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		k++;
+	}
+	for (n = (k -1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
