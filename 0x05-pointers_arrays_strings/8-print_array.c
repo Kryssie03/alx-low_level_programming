0f5103a2d11e1c71c0248a6147bb619304a0da26
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-19 17:47:13 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function to print elements of array 
+ * @a: function parameters
+ * @n: function parameters
+ * Description: Numbers should be printed accordingly and separated by comma
+ */
+
+void print_array(int *a, int n)
+{
+	int a, i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar(',');
+	_putchar(' ');
+	_putchar('\n');
+}
