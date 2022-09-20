cf703db109bfe3bedb65c2a37c4d6f2ce8f9920a
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-19 17:06:50 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print one character out of others
+ * @str: function parameter
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+			continue;
+	}
+}
