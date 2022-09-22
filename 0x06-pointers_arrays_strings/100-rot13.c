b15ef2ab6f1bf2e61f30a2a38aecb3fbc656e43a
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-22 16:55:43 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,21 +4,21 @@
 /**
  * rot13 - function to encode a string using rot13
  * @n: input
- * Return: 0
+ * Return: *n
  */
 
 char *rot13(char *n)
 {
 	int i;
 	int k;
-	int wr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	int w[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	int rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (k = 0; k < 52; k++)
 		{
-			if (n[i] == wr[k])
+			if (n[i] == w[k])
 			{
 				n[i] = rot[k];
 				break;
