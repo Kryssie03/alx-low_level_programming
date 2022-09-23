c08263ecc7ed715dd76f0f38a5817f681c021543
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-22 17:02:16 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *n)
 {
 	int i, j;
 	int capt = 32;
-	int separators[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ',  '\n', '\t'};
+	int separators[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
