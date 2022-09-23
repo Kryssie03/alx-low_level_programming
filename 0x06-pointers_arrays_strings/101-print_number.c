63165a931ecc8d5e17bcdeefd71f6430213eced1
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-22 17:52:23 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,25 +15,14 @@ void print_number(int n)
 		_putchar('-');
 		num = -n;
 	}
-	else if (n < 10)
+	else
 	{
-		_putchar(n);
+		num = n;
 	}
-	else if (n >= 10)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n >= 100)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n >= 1000)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 1000) % 100) + '0');
-		_putchar((((n / 1000) % 100) % 10) + '0');
-	}
+
+       	if (n / 10)
+
+		print_number(num / 10);
+
+	_putchar(num % 10 + '0');
 }
