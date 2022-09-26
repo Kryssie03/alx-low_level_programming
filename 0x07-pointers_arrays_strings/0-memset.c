991a7677c21f9a2e2b14a6a3fe34cc80a7b331f0
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-26 15:59:19 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - function to fill first n characters with a constant value 'b'
+ * @s: arrayname
+ * @b: value to be filled
+ * @n: number of bytes to fill
+ *
+ * Return: a pointer
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		char s[i] = b;
+	}
+
+	return (s);
+}
