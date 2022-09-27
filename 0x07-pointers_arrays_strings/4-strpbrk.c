114089d10064d124e0d9d84a2b3d9ec529dc3d0f
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-26 18:07:52 -0700

    pointers is fun

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes.
+ * @s: string to search
+ * @accept: string to check against
+ *
+ * Return: a pointer to the byte in s that matches one of the bytes in accept
+ * or null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+		if (accept[j])
+			return (s + i);
+	}
+	return (0);
+}
