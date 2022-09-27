0049dd496d7f72241febe26e21d58082e985228f
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-26 17:08:27 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: input
+ * @c: character to be located
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == 'c')
+		{
+			return (s);
+		}
+		else
+			return (NULL);
+	}
+}
