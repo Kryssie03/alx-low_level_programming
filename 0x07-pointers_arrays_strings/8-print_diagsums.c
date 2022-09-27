b0d5972e1e219da42c8afd4c51a79b954cf4aaaa
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-26 20:23:54 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,17 +12,17 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, k = 0, l = 0;
+	int i, j, p, l = 0, r = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		p = (i * size) + i;
-		k += *(a + p);
+		l += *(a + p);
 	}
 	for (j = 0; j < size; j++)
 	{
 		p = (j * size) + (size - 1 - j);
-		l += *(a + p);
+		r += *(a + p);
 	}
 	printf("%d, %d\n", l, r);
 }
