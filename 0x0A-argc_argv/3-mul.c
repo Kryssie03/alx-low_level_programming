21285a116e08cc748c2ed27614a97f26509d3a18
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-30 18:47:49 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,7 +6,7 @@
  * @argc: number of arguements passed to the function
  * @argv: arguement vectors of pointers to strings
  *
- * Return: 0
+ * Return: 0 if no errors else 1
  */
 
 int main(int argc, char *argv[])
@@ -16,7 +16,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	
 	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
