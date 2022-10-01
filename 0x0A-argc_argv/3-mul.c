1bc5cbe03329bce80a461f9b96872a6bcec72a82
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-30 18:45:29 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to multiply two numbers
+ * @argc: number of arguements passed to the function
+ * @argv: arguement vectors of pointers to strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
