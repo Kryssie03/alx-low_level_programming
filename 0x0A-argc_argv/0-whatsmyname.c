642c881f71a2005b30ab370f9cc95238138c3b98
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-30 18:06:41 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguements passed go the function
+ * @argv: arguements vectors of pointers to the string
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
