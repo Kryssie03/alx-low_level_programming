76ffd54bc96fd7e90812b2e82e19472fc3387999
Kryssie03 <kosisockukwuakabekwa@gmail.com>
2022-09-30 18:16:39 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function to print the number of arguements passed to it
+ * @argc: number of arguements passed to the function
+ * @argv: arguements vectors of pointers to string
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
